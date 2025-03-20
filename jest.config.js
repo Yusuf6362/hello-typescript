277ee575ae8d0ba10591cb7b9b0dd62a07e32e71
Yusuf Er <yusuf_er@sluz.ch>
2025-03-20 17:59:44 +0100

    generate source maps for debugging

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+export const roots = ["src"];
+export const transform = { "^.+\\.tsx?$": "ts-jest" };
+
---
+++ src/rounding.ts
@@ -0,0 +1,7 @@
+export function roundTo(x: number, granularity: number): number {
+  const factor = 1.0 / granularity;
+  return Math.round(x * factor) / factor;
+}
+
+
+
